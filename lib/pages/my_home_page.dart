b88864a8997eff9102727e1d7e645700f9ea68c6
Diogo-Freitas <diogofreitasdf@hotmail.com>
2024-12-06 09:55:49 -0300

    Requisição de Dados da API do GIPHY com HTTP

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -11,13 +13,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    super.initState();
+    getResponse('dogs').then((map) {
+      print(map);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.black,
-          title: Text(widget.title, style: const TextStyle(color: Colors.white)),
+          title:
+              Text(widget.title, style: const TextStyle(color: Colors.white)),
         ),
         body: const Center());
   }
+}
+
+Future<Map> getResponse([String? search]) async {
+  late Uri uri;
+
+  if (search == null) {
+    uri = Uri.https(
+      'api.giphy.com',
+      '/v1/gifs/trending',
+      {
+        'api_key': 'qHxJdD1Hrcw4MToDx5g9ThuIJpaJJbSq',
+        'limit': '20',
+        'offset': '0',
+        'rating': 'g',
+        'bundle': 'messaging_non_clips',
+      },
+    );
+  } else {
+    uri = Uri.https(
+      'api.giphy.com',
+      '/v1/gifs/search',
+      {
+        'api_key': 'qHxJdD1Hrcw4MToDx5g9ThuIJpaJJbSq',
+        'limit': '20',
+        'offset': '0',
+        'rating': 'g',
+        'bundle': 'messaging_non_clips',
+        'q': search,
+      },
+    );
+  }
+
+  var response = await http.get(uri);
+
+  return json.decode(response.body);
 }
